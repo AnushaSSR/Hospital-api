895f6a589bb691f404f5f09ca635e8c01013ff5a
Anusha S S R <nssranusha@yahoo.co.in>
2022-10-23 22:43:32 +0530

    POrt env variable added

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8000;
-const db = require('./config/mongoose');
-const passport = require('passport');
+const port = process.env.PORT || 8000;
+const db = require("./config/mongoose");
+const passport = require("passport");
 //jwt startegy config file
-const passportJWT = require('./config/passport-jwt-strategy');
+const passportJWT = require("./config/passport-jwt-strategy");
 
 // to parse data from from
 app.use(express.urlencoded());
 
 // use the routes folder
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 //listen to port
 app.listen(port, function (err) {
-    if (err) {
-        //if any error
-        console.log(`Error in loading server ${err}`);
-    } else {
-        //if up and running
-        console.log(`Server is up and running in port :: ${port}`);
-    }
+  if (err) {
+    //if any error
+    console.log(`Error in loading server ${err}`);
+  } else {
+    //if up and running
+    console.log(`Server is up and running in port :: ${port}`);
+  }
 });
