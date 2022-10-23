d621b93fbeff5a4c2bbc8eec04fdc6e2bc79791b
Anusha S S R <nssranusha@yahoo.co.in>
2022-10-24 00:06:32 +0530

    error message modified

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.use('/doctors', require('./doctors'));//use doctors route
-router.use('/patients', require('./patients'));//use patients route
-router.use('/reports', require('./reports'));//use reports route
+router.use("/doctors", require("./doctors")); //use doctors route
+router.use("/patients", require("./patients")); //use patients route
+router.use("/reports", require("./reports")); //use reports route
 
 //Message to user when trying to access with the / route
-router.all('/', function(req,res) {
-    return res.json(400, {
-        message: 'route not available. Check out the documentation for valid routes'
-    });
+router.all("/", function (req, res) {
+  return res.json(400, {
+    message:
+      'route not available. Check out the documentation in "https://github.com/AnushaSSR/Hospital-api/edit/main/README.md" for valid routes',
+  });
 });
 
 module.exports = router;
